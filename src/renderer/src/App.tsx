ced8d7c9813df57db826364b276b3dff8ce64c98
junenghia <nguyenminhtrungnghia@gmail.com>
2024-08-30 01:23:45 +0700

    update code

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,47 +1,44 @@
-// import { RouterProvider } from "react-router-dom";
-// import router from "./routes";
-// import { Suspense, useEffect, useState } from "react";
-// import Loader from "./components/Loader";
-// import { Slide, ToastContainer } from "react-toastify";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { Suspense, useEffect, useState } from "react";
+import Loader from "./components/Loader";
+import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Error from "./components/Error";
-// import { checkHealth } from "./services/health.service";
-import UserPage from "./components/User";
+import Error from "./components/Error";
+import { checkHealth } from "./services/health.service";
 
 function App(): JSX.Element {
-  // const [isOK, setIsOK] = useState(true);
+  const [isOK, setIsOK] = useState(true);
 
-  // useEffect(() => {
-  //   checkHealth().catch(() => {
-  //     setIsOK(false);
-  //   });
-  // }, []);
+  useEffect(() => {
+    checkHealth().catch(() => {
+      setIsOK(false);
+    });
+  }, []);
 
-  // if (!isOK) {
-  //   return <Error />;
-  // }
+  if (!isOK) {
+    return <Error />;
+  }
 
   return (
-    // <Suspense fallback={<Loader />}>
-    //   <RouterProvider router={router} />
+    <Suspense fallback={<Loader />}>
+      <RouterProvider router={router} />
 
-    //   <ToastContainer
-    //     position="top-center"
-    //     autoClose={3000}
-    //     hideProgressBar={false}
-    //     newestOnTop={false}
-    //     closeOnClick
-    //     rtl={false}
-    //     pauseOnFocusLoss
-    //     draggable
-    //     pauseOnHover
-    //     theme="colored"
-    //     limit={1}
-    //     transition={Slide}
-    //   />
-    // </Suspense>
-
-    <UserPage />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        limit={1}
+        transition={Slide}
+      />
+    </Suspense>
   );
 }
 
